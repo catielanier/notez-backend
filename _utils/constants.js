0c884dfe637301b23f7ff66138bc73288a5534b0
Corey Lanier <iam@coreylanier.com>
2021-09-12 23:23:19 -0400

    setup deploy to heroku

---
+++ _utils/constants.js
@@ -0,0 +1,5 @@
+exports.URL = process.env.MONGODB_URI;
+exports.PORT = process.env.PORT;
+exports.SECRET = process.env.SECRET;
+exports.MJ_APIKEY_PUBLIC = process.env.MJ_APIKEY_PUBLIC;
+exports.MJ_APIKEY_PRIVATE = process.env.MJ_APIKEY_PRIVATE;
