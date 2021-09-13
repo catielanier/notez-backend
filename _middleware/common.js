0a36283b35582b17aae912cdfd9ccad51383183f
Corey Lanier <iam@coreylanier.com>
2021-09-13 00:45:25 -0400

    add cors settings

--- _middleware/common.js
+++ _middleware/common.js
@@ -1,6 +1,16 @@
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
-exports.handleBodyRequestParsing = router => {
+const corsOptions = {
+  origin: [
+    "https://checkthenotez.com",
+    "https://www.checkthenotez.com",
+    "http://localhost:3000",
+  ],
+};
+
+exports.handleBodyRequestParsing = (router) => {
   router.use(bodyParser.urlencoded({ extended: true }));
   router.use(bodyParser.json());
+  router.use(cors(corsOptions));
 };
